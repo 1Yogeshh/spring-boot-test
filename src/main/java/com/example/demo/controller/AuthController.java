3be1fe3713252015dfbe0facc7f0ee3e06983f3c
Yogesh Kumar <117355987+1Yogeshh@users.noreply.github.com>
2025-09-18 17:12:28 +0530

    add

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -4,13 +4,9 @@ import com.example.demo.model.User;
 import com.example.demo.service.AuthService;
 import com.example.demo.service.UserDetailServiceImp;
 import com.example.demo.utils.jwtUtils;
-
 import dto.UserDTO;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
-
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -52,17 +48,13 @@ public class AuthController {
     // Login
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody User loginUser) {
-        // return service.login(loginUser.getUsername(), loginUser.getPassword());
         try {
             authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(loginUser.getUsername(), loginUser.getPassword()));
             UserDetails userDetails = userDetailServiceImp.loadUserByUsername(loginUser.getUsername());
-
             String token = jwtUtils.generateToken(userDetails.getUsername());
-            System.out.println("JWT Token: " + token);
             return ResponseEntity.ok(token);
         } catch (Exception e) {
-            // TODO: handle exception
             log.error("Exception occurred while createAuthenticationToken ", e);
             return new ResponseEntity<>("Incorrect username or password", HttpStatus.BAD_REQUEST);
         }
