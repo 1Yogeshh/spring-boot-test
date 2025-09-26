64f007d3bac532f787924a042464eb08b13c98ae
Yogesh Kumar <117355987+1Yogeshh@users.noreply.github.com>
2025-09-26 11:09:05 +0530

    hello

--- src/main/java/com/example/demo/controller/AuthsController.java
+++ src/main/java/com/example/demo/controller/AuthsController.java
@@ -39,7 +39,7 @@ public class AuthsController {
     @Autowired
     private RedisService redisService;
 
-    // login endpoint for jwt
+    // login endpoint for jwt for
     @PostMapping("/login")
     public String login(@RequestBody User loginUser) {
         authenticationManager.authenticate(
